a83f2696004bc13e4fcd8b1e5bcba8ba0cfb2cfd
Collins-Webdev <collinshaya@gmail.com>
2024-02-22 21:18:20 +0100

    Made important useful changes to the code

--- tests/unit/fs/test_fs.py
+++ tests/unit/fs/test_fs.py
@@ -1,16 +1,20 @@
 import pytest
+from dvc import config
+from dvc.config import RemoteNotFoundError
+from dvc.fs import LocalFileSystem, get_cloud_fs, get_fs_cls, get_fs_config
 from dvc_http import HTTPFileSystem, HTTPSFileSystem
 from dvc_s3 import S3FileSystem
 from dvc_ssh import SSHFileSystem
-from dvc.config import RemoteNotFoundError
-from dvc.fs import LocalFileSystem, get_cloud_fs, get_fs_cls, get_fs_config
 
-# List of URL and corresponding FileSystem classes
+# Add docstrings to functions and classes
+
 url_cls_pairs = [
+    # Cloud-based file systems
     ("s3://bucket/path", S3FileSystem),
     ("ssh://example.com:/dir/path", SSHFileSystem),
     ("http://example.com/path/to/file", HTTPFileSystem),
     ("https://example.com/path/to/file", HTTPSFileSystem),
+    # Local file system
     ("path/to/file", LocalFileSystem),
     ("path\\to\\file", LocalFileSystem),
     ("file", LocalFileSystem),
@@ -21,35 +25,38 @@ url_cls_pairs = [
     ("unknown://path", LocalFileSystem),
 ]
 
-# Include HDFSFileSystem if available
 try:
     from dvc_hdfs import HDFSFileSystem
-
     url_cls_pairs += [("hdfs://example.com/dir/path", HDFSFileSystem)]
 except ImportError:
-    pass
+    HDFSFileSystem = None
 
+# Use more descriptive names for test functions
+@pytest.mark.parametrize("url, expected_cls", url_cls_pairs)
+def test_get_fs_cls(url, expected_cls):
+    """
+    Test if the correct filesystem class is returned for a given URL.
+    """
+    assert get_fs_cls({"url": url}) == expected_cls
 
-# Test for get_fs_cls function
-@pytest.mark.parametrize("url, cls", url_cls_pairs)
-def test_get_fs_cls(url, cls):
-    assert get_fs_cls({"url": url}) == cls
-
-
-# Test for get_fs_config function
 def test_get_fs_config():
+    """
+    Test the get_fs_config function.
+    """
     result = get_fs_config({}, url="ssh://example.com:/dir/path")
     assert result == {"url": "ssh://example.com:/dir/path"}
 
-
-# Test for get_fs_config function with error handling
 def test_get_fs_config_error():
+    """
+    Test get_fs_config when RemoteNotFoundError is raised.
+    """
     with pytest.raises(RemoteNotFoundError):
         get_fs_config({"remote": {}}, name="myremote")
 
-
-# Test for remote_url function
 def test_remote_url():
+    """
+    Test remote URL creation from configuration.
+    """
     config = {
         "remote": {
             "base": {"url": "http://example.com"},
@@ -64,9 +71,10 @@ def test_remote_url():
         "url": "http://example.com/r1/r2/foo",
     }
 
-
-# Test for get_cloud_fs function
 def test_get_cloud_fs():
+    """
+    Test get_cloud_fs function.
+    """
     cls, config, path = get_cloud_fs({}, url="ssh://example.com:/dir/path")
     assert cls is SSHFileSystem
     assert config == {"host": "example.com", "verify": False}
